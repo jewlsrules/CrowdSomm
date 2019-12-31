93d76e40f6dc934fadd1b397e6abd30c36aa2cb6
Jewls Krueger <Juliarkrueger@gmail.com>
2019-12-31 12:40:46 -0800

    started working on sessions controller and views

---
+++ controllers/sessions.js
@@ -0,0 +1,35 @@
+//----------------------
+// Dependencies
+//----------------------
+const express = require('express');
+const router = express();
+const methodOverride = require('method-override') //convert strings in forms
+const mongoose = require('mongoose') //for database
+const session = require('express-session') //for cookies
+const bcrypt = require('bcrypt') //for password encryption
+const Project = require('../models/project.js')
+const User = require('../models/user.js')
+
+invalid = false;
+
+//----------------------
+// Routes
+//----------------------
+
+// Show Log In Page
+router.get('/login', (req, res) => {
+  if(!req.session.username){
+    res.render('sessions/login.ejs', {
+      invalid: invalid
+    })
+  } else {
+    res.redirect('/')
+  }
+}) // end of show log in page
+
+
+
+//----------------------
+// Export
+//----------------------
+module.exports = router;
