394b31bb288f11a0fd601d29186a90942ea8750e
Jewls Krueger <Juliarkrueger@gmail.com>
2020-01-06 14:28:41 -0800

    can post a new review successfully, but redirect isn't working right

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -22,6 +22,8 @@ require('dotenv').config()
 const YELP_API_KEY = process.env.YELP_API_KEY
 const client = yelp.client(YELP_API_KEY)
 
+let request1
+
 //show restaurants page
 router.get('/', (req, res) => {
   console.log('controller working');
@@ -29,6 +31,8 @@ router.get('/', (req, res) => {
     user: req.session.user
   })
 }) // end of show sign up page
+
+let repos1
 
 //show individual restaurant page
 router.get('/:id', (req, res) => {
@@ -40,84 +44,31 @@ router.get('/:id', (req, res) => {
       // console.log("2. ", response.jsonBody);
       req.session.restaurant = response.jsonBody;
       //using the options from above, get the data from php backend
-      rp({uri: 'https://crowdsommphp.herokuapp.com/api/reviews/restid/'+req.params.id, json: true}).then(function (repos) {
-        // console.log('repos: ', repos); // Print the responses
-        req.session.reviews = repos
-        // console.log('repos are here!', repos);
-      }).then(() => {
-        //this is getting just the rating and id so we can get the averages.
-        let array1 = req.session.reviews;
-        array1.sort();
-        let reviews_ratings = array1.map(item => {
-          const container = {};
-          container.dish_id = item.dish_id;
-          container.stars = item.stars;
-          container.dish_name = item.dish_name
-          return container
+      rp({uri: 'https://crowdsommphp.herokuapp.com/api/dishes/restid/'+req.params.id, json: true})
+      .then(function (repos) {
+        if(!repos){
+          repos1 = 'no dishes'
+        } else {
+          repos1 = repos
+        }
+        console.log('repos are here!', repos1);
+      })
+      .then(() => {
+        let findAverages = (item) => {
+          // console.log(item.id);
+          rp({uri: 'https://crowdsommphp.herokuapp.com/api/reviews/dishid/averagestars/'+item.id, json: true})
+        .then(function (repos2) {
+          // console.log('repos2 ', repos2);
         })
-        console.log(reviews_ratings);
-        let finals = [];
-        let findaverages = () => {
-          let dish_name_tracker = reviews_ratings[0].dish_name;
-          let total_sum = 0;
-          let dish_id_tracker = reviews_ratings[0].dish_id;
-          let counter = 0;
-          for(let i = 0; i<(reviews_ratings.length); i++){
-            console.log(i+' - dish_id:'+ reviews_ratings[i].dish_id);
-            // if the dish_id matches the tracker, add the stars to the sum.
-            if(i==(reviews_ratings.length-1)){
-              total_sum += reviews_ratings[i].stars;
-              counter++
-              dish_name_tracker = reviews_ratings[i].dish_name;
-              dish_id_tracker = reviews_ratings[i].dish_id
-              const container1 = {};
-              container1.dish_name = dish_name_tracker;
-              container1.dish_id = dish_id_tracker;
-              container1.sum = total_sum;
-              container1.counter = counter;
-              container1.avgStars = total_sum/counter;
-              finals.push(container1);
-              dish_id_tracker = reviews_ratings[i].dish_id;
-              total_sum = reviews_ratings[i].stars;
-              counter = 1;
-              console.log(i+'- last one!');
-              //if the ID matches a previous one, just increase the sum and counter.
-            } else if(reviews_ratings[i].dish_id === dish_id_tracker){
-              //add the star rating to the sum.
-              console.log(i+' - review stars:'+ reviews_ratings[i].stars+' totalsum:'+total_sum+' counter='+counter);
-              total_sum += reviews_ratings[i].stars;
-              counter++
-              //if it's a new one,
-            } else {
-              const container1 = {};
-              container1.dish_name = dish_name_tracker;
-              container1.dish_id = dish_id_tracker;
-              container1.sum = total_sum;
-              container1.counter = counter;
-              container1.avgStars = total_sum/counter;
-              finals.push(container1);
-              dish_id_tracker = reviews_ratings[i].dish_id;
-              total_sum = reviews_ratings[i].stars;
-              counter = 1;
-              console.log(i+' - no match!');
-              // console.log(finals);
-            }
-            console.log(finals);
-          }
         }
-        //run the above function
-        if (reviews_ratings[0]) {
-          findaverages();
-        }
-        // console.log('page loading now');
-        // console.log('user cookie: ', req.session.user);
+        repos1.forEach(findAverages)
+      })
+      .then(() => {
+        // console.log('repos1 = ', repos1);
           res.render('restaurants/show.ejs', {
-            restaurant_name: req.session.restaurant.name,
-            restaurant_address: req.session.restaurant.location[0],
-            restaurant_id: req.session.restaurant.id,
+            restaurant: req.session.restaurant,
             user: req.session.user,
-            reviews: req.session.reviews,
-            averages: finals
+            dishes: repos1
           })
       })
     }).catch(e => {
@@ -142,41 +93,37 @@ router.get('/:id/newreview', (req, res) => {
       })
     }).catch(e => {
       console.log("this is the error: ", e);
-    });;
+    });
 }) // end of new review show page
 
-//show page for adding a new review on an existing dish
-router.get('/:restaurantid/:dishid', (req, res) => {
+//show page for adding a new dish to a restaurant
+router.get('/:restaurantid/newdish', (req, res) => {
   // req.session.restuarant_id = req.params.restaurantid;
-  // console.log(req.session);
-  rp({uri: 'https://crowdsommphp.herokuapp.com/api/reviews/restid/'+req.params.restuarantid, json: true}).then(function (repos) {
-    // console.log('repos: ', repos); // Print the responses
-    req.session.reviews = repos
-    // console.log('repos are here!', repos);
-  }).then(() => {
-    res.render('restaurants/review.ejs', {
-      restaurant_id: req.params.restaurantid,
-      restaurant_name: req.session.restaurant.name,
-      dish_id: req.params.dishid,
-      user: req.session.user,
-    })
-  })
+  client.business(req.params.restaurantid)
+    .then(response => {
+      console.log('yelp api call');
+      console.log("full response: ", response.jsonBody);
+      req.session.restuarant = response.jsonBody;
+    }).then(() => {
+      console.log('page loading now');
+      res.render('restaurants/newdish.ejs', {
+        restaurant_id: req.params.restaurantid,
+        restaurant_name: req.session.restaurant.name,
+        user: req.session.user,
+      })
+    }).catch(e => {
+      console.log("this is the error: ", e);
+    });
 }) // end of new review show page
 
-let request1
-
-router.post('/:id/newreview', (req, res) => {
+router.post('/:id/newdish', (req, res) => {
   console.log(req.body); //this returns an object based on the names of the inputs
   request1 = {
       method: 'POST',
-      uri: 'https://crowdsommphp.herokuapp.com/api/reviews',
+      uri: 'https://crowdsommphp.herokuapp.com/api/dishes',
       body: {
-          user_id: req.session.user._id,
-          restaurant_id: req.params.id,
           dish_name: req.body.dish_name,
-          dish_id: 875, //we need to change this!!
-          stars: req.body.stars,
-          review_text: req.body.review_text
+          restaurant_id: req.params.id,
       },
       json: true // Automatically stringifies the body to JSON
   };
@@ -184,7 +131,6 @@ router.post('/:id/newreview', (req, res) => {
   rp(request1)
     .then(function (parsedBody) {
       console.log('success! i think.');
-        // POST succeeded...
     })
     .then(() => {
       res.redirect('/restaurants/'+req.params.id)
@@ -194,6 +140,64 @@ router.post('/:id/newreview', (req, res) => {
     });
 });
 
+//show page for adding a new review on an existing dish
+router.get('/:restaurantid/:dishid', (req, res) => {
+  // req.session.restuarant_id = req.params.restaurantid;
+  // console.log(req.session);
+  rp({uri: 'https://crowdsommphp.herokuapp.com/api/dishes/dishid/'+req.params.dishid, json: true}).then(function (repos) {
+    // console.log('repos: ', repos); // Print the responses
+    repos1 = repos
+    console.log('repos are here!', repos);
+  }).then(() => {
+    res.render('restaurants/review.ejs', {
+      restaurant_id: req.params.restaurantid,
+      restaurant_name: req.session.restaurant.name,
+      dish: repos1,
+      dish_id: req.params.dishid,
+      user: req.session.user
+    })
+  })
+}) // end of new review show page
+
+router.post('/newreview/:restaurantid/:dishid', (req, res) => {
+  console.log(req.body); //this returns an object based on the names of the inputs
+  rp({uri: 'https://crowdsommphp.herokuapp.com/api/dishes/dishid/'+req.params.dishid, json: true}).then(function (repos) {
+    // console.log('repos: ', repos); // Print the responses
+    repos1 = repos
+    console.log('repos are here!', repos);
+  })
+  .then(() => {
+    request1 = {
+        method: 'POST',
+        uri: 'https://crowdsommphp.herokuapp.com/api/reviews',
+        body: {
+            user_id: req.session.user._id,
+            restaurant_id: req.params.restaurantid,
+            dish_name: repos1[0].dish_name,
+            dish_id: req.params.dishid,
+            stars: req.body.stars,
+            review_text: req.body.review_text
+        },
+        json: true // Automatically stringifies the body to JSON
+    };
+    console.log(request1);
+  })
+  .then(() => {
+    rp(request1)
+      .then(function (parsedBody) {
+        console.log('success! i think.');
+          // POST succeeded...
+      })
+      .then(() => {
+        res.redirect('/restaurants/'+req.params.restuarantid)
+      })
+      .catch(e => {
+        console.log("this is the error: ", e);
+      });
+  })
+});
+
+//delete review route
 router.get('/:restuarant_id/:review_id', (req, res) => {
   request1 = {
     method: 'DELETE',
@@ -216,3 +220,73 @@ router.get('/:restuarant_id/:review_id', (req, res) => {
 // Export
 //----------------------
 module.exports = router;
+
+//----------------------
+// Graveyard - RIP
+//----------------------
+// //this is getting just the rating and id so we can get the averages.
+// let array1 = req.session.reviews;
+// array1.sort();
+// let reviews_ratings = array1.map(item => {
+//   const container = {};
+//   container.dish_id = item.dish_id;
+//   container.stars = item.stars;
+//   container.dish_name = item.dish_name
+//   return container
+// })
+// console.log(reviews_ratings);
+// let finals = [];
+// let findaverages = () => {
+//   let dish_name_tracker = reviews_ratings[0].dish_name;
+//   let total_sum = 0;
+//   let dish_id_tracker = reviews_ratings[0].dish_id;
+//   let counter = 0;
+//   for(let i = 0; i<(reviews_ratings.length); i++){
+//     console.log(i+' - dish_id:'+ reviews_ratings[i].dish_id);
+//     // if the dish_id matches the tracker, add the stars to the sum.
+//     if(i==(reviews_ratings.length-1)){
+//       total_sum += reviews_ratings[i].stars;
+//       counter++
+//       dish_name_tracker = reviews_ratings[i].dish_name;
+//       dish_id_tracker = reviews_ratings[i].dish_id
+//       const container1 = {};
+//       container1.dish_name = dish_name_tracker;
+//       container1.dish_id = dish_id_tracker;
+//       container1.sum = total_sum;
+//       container1.counter = counter;
+//       container1.avgStars = total_sum/counter;
+//       finals.push(container1);
+//       dish_id_tracker = reviews_ratings[i].dish_id;
+//       total_sum = reviews_ratings[i].stars;
+//       counter = 1;
+//       console.log(i+'- last one!');
+//       //if the ID matches a previous one, just increase the sum and counter.
+//     } else if(reviews_ratings[i].dish_id === dish_id_tracker){
+//       //add the star rating to the sum.
+//       console.log(i+' - review stars:'+ reviews_ratings[i].stars+' totalsum:'+total_sum+' counter='+counter);
+//       total_sum += reviews_ratings[i].stars;
+//       counter++
+//       //if it's a new one,
+//     } else {
+//       const container1 = {};
+//       container1.dish_name = dish_name_tracker;
+//       container1.dish_id = dish_id_tracker;
+//       container1.sum = total_sum;
+//       container1.counter = counter;
+//       container1.avgStars = total_sum/counter;
+//       finals.push(container1);
+//       dish_id_tracker = reviews_ratings[i].dish_id;
+//       total_sum = reviews_ratings[i].stars;
+//       counter = 1;
+//       console.log(i+' - no match!');
+//       // console.log(finals);
+//     }
+//     console.log(finals);
+//   }
+// }
+// //run the above function
+// if (reviews_ratings[0]) {
+//   findaverages();
+// }
+// console.log('page loading now');
+// console.log('user cookie: ', req.session.user);
